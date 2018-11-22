be713fd734b72b0683ba03bf1aecd1a697e16616
Foram <foramdadhania16@gmail.com>
2018-11-22 09:31:32 -0500

    - Initial commit

---
+++ FGNotes/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FGNotes
+//
+//  Created by Foram Dadhania on 2018-11-21.
+//  Copyright © 2018 Foram Dadhania. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func addNote(_ sender: Any) {
+        
+    }
+    
+
+}
+
